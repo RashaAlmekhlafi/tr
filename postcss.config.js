02a18b06cb8841d180c732b0f365baaea25dbd3f
Reem Abdulglil Hassan Saleh <reemabdulglilhassansaleh@Reems-MacBook-Air.local>
2025-04-20 20:51:07 +0300

    first commit2

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // استخدمي الباكيج الجديد
+    autoprefixer: {},
+  },
+};
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // إذا كنتِ تستخدمين App Router
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Tajawal", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
